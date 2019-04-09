4eb6df606be1170b9e509bb68cbee9287b4d43ee
Andrew Wong <andrew.j.wong@outlook.com>
2019-04-09 14:29:56 +1000

    Add loader

---
+++ bot.js
@@ -0,0 +1,102 @@
+const config = require("./.config.js");
+const libfb = require("libfb");
+const fs = require("fs");
+
+(async () => {
+  let bot;
+  try {
+    // Login
+    bot = await libfb.login(
+      config.facebook_username,
+      config.facebook_password,
+      {
+        selfListen: true,
+        // Read session if exists
+        session: fs.existsSync(__dirname + "/.appstate.json")
+          ? JSON.parse(fs.readFileSync(__dirname + "/.appstate.json", "utf8"))
+          : undefined
+      }
+    );
+  } catch (err) {
+    // Login errors
+    let error_title = err.errorData.error_title;
+    let error_message = err.errorData.error_message;
+    console.error(`Error: ${error_title} - "${error_message}"`);
+    process.exit(1);
+  }
+
+  // Store session
+  fs.writeFileSync(
+    __dirname + "/.appstate.json",
+    JSON.stringify(bot.getSession())
+  );
+
+  bot.id = bot.session.tokens.uid;
+  bot.admins = config.bot_admins;
+  console.log(`User ID: ${bot.id}\nAdmins: ${bot.admins}`);
+
+  let _commandNames = ["help"];
+  let commands = {};
+  for (let command of _commandNames) {
+    commands[command] = require(`./commands/${command}`)(bot);
+    if (typeof commands[command].name === "string") {
+      commands[command].name = Array(commands[command].name);
+    }
+  }
+  let commandMap = {};
+  for (let command in commands) {
+    for (let name of commands[command].name) {
+      commandMap[name.toLowerCase()] = commands[command];
+    }
+  }
+
+  bot.on("message", message => {
+    // {
+    //     threadId: int,
+    //     attachments: array,
+    //     authorId: int,
+    //     id: str',
+    //     timestamp: epoch,
+    //     message: str,
+    //     stickerId: int
+    // }
+
+    // Check if the message starts with the command prefix
+    if (!message.message.startsWith(config.bot_command_prefix)) return;
+
+    // Break down
+    let tokens = message.message.split(" ");
+    let commandStr = tokens[0]
+      .toLowerCase()
+      .replace(config.bot_command_prefix.toLowerCase(), "");
+
+    // Check of the command exists
+    if (commandStr in commandMap) {
+      // Check if the user has permission to run the command
+      if (commandMap[commandStr].admin && !(message.authorId in bot.admins)) {
+        bot.sendMessage(
+          message.threadId,
+          "Error: You do not have permission to execute this command!"
+        );
+        return;
+      }
+
+      // Try run the command
+      try {
+        let response = commandMap[commandStr].function(
+          tokens.slice(1).join(" ")
+        );
+        if (response) {
+          bot.sendMessage(message.threadId, response);
+        }
+        // Catch exception messages
+      } catch (err) {
+        bot.sendMessage(message.threadId, `Error: ${err}`);
+      }
+    } else {
+      // Command not found!!
+    }
+  });
+
+  console.info("Loading complete");
+})();
